273adf4eded8bb0aad1e99b38aa7ade2dc09a7ca
Radium <2043869032@qq.com>
2021-06-04 15:27:33 +0800

    fixed 提交

--- packages/carousel/index.ts
+++ packages/carousel/index.ts
@@ -1,8 +1,14 @@
 import {App} from 'vue'
-import { SFCWithInstall } from '@radium-vue/utils/types';
+import { SFCWithInstall } from '@radium-vue/';
 import Carousel from './src/carousel.vue';
+import { Cascader } from 'element-ui';
 
 
 Carousel.install = (app:App): void => {
     app.component(Carousel.name,Carousel)
 }
+
+
+const _Carousel:SFCWithInstall<typeof Cascader> = Cascader;
+
+export default _Carousel;
