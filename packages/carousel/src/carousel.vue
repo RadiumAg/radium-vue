060288ca25e9a33818eecfc1c97919510c69ce59
Radium <2043869032@qq.com>
2021-06-04 18:28:36 +0800

    提交一下

--- packages/carousel/src/carousel.vue
+++ packages/carousel/src/carousel.vue
@@ -4,7 +4,9 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import { CarouselProp } from './carousel';
 export default defineComponent({
   name: 'Carousel',
+  setup(props: CarouselProp) {},
 });
 </script>
