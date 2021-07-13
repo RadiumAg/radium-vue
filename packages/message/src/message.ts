72fd644fca69a9bfccf15ccc7e1af60a2ec902ad
zly <2043869032@qq.com>
2021-07-13 20:46:20 +0800

    commit

--- packages/message/src/message.ts
+++ packages/message/src/message.ts
@@ -18,34 +18,46 @@ export class Message {
   private static instanceArray: { vm: VNode; id: symbol }[] = [];
 
   static getOffsetVertical(raOffset: number) {
-    const offsetVertical = raOffset || 20;
     let result = 0;
-    if (this.instanceArray.length)
-      this.instanceArray.forEach(obj => {
-        result =
-          result + offsetVertical + (obj.vm.el as HTMLElement).offsetHeight;
+    if (this.instanceArray.length) {
+      this.instanceArray.forEach((obj, index) => {
+        const preInstanceOffsetHeight = (this.instanceArray[index].vm
+          .el as HTMLElement).offsetHeight;
+        if (this.instanceArray.length && index === 0)
+          result = result + raOffset * 2 + preInstanceOffsetHeight;
+        else result = result + raOffset + preInstanceOffsetHeight;
       });
-    elseresult + offsetVertical + (obj.vm.el as HTMLElement).offsetHeight;
+    } else result = raOffset;
     return result;
   }
 
   static restartTheOffset(id: symbol, offset: number) {
     const targetInstanceIndex = this.instanceArray.findIndex(_ => _.id === id);
+    let result = 0;
     this.instanceArray.splice(targetInstanceIndex, 1);
     this.instanceArray.forEach((v, index) => {
-      v.vm.el.style.top = index * offset + 'px';
+      let preInstanceOffsetHeight = 0;
+      if (index > 0) {
+        preInstanceOffsetHeight = (this.instanceArray[index - 1].vm
+          .el as HTMLElement).offsetHeight;
+        result = result + preInstanceOffsetHeight + offset;
+      } else if (index === 0) {
+        result = offset;
+      }
+      v.vm.el.style.top = result + 'px';
     });
   }
 
   static create(options: TMessageOptions) {
     const container = document.createElement('div');
-    const offsetVertical = this.getOffsetVertical(options.raOffset);
+    const offsetVertical = options.raOffset || 20;
+    const offset = this.getOffsetVertical(offsetVertical);
     const symbol = Symbol();
     const vm = createVNode(
       MessageConstructor,
       {
         ...options,
-        raOffset: offsetVertical,
+        raOffset: offset,
         raType: options.raType,
       },
       isVNode(options.raMessage) ? { default: () => options.raMessage } : null,
@@ -53,7 +65,7 @@ export class Message {
 
     vm.props.onRaDestroy = () => {
       render(null, container);
-      this.restartTheOffset(symbol, options.raOffset || 60);
+      this.restartTheOffset(symbol, offsetVertical);
     };
 
     this.instanceArray.push({ vm, id: symbol });
