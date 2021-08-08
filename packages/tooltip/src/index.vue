3ac0480f6315d8722a156d00bb17dc3ab987a5b9
zly <2043869032@qq.com>
2021-08-08 23:30:41 +0800

    commit

--- packages/tooltip/src/index.vue
+++ packages/tooltip/src/index.vue
@@ -4,10 +4,36 @@
   </div>
 </template>
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, PropType } from 'vue';
+import { TPlacement, TToolTipProps } from '.';
 export default defineComponent({
   name: 'RaTooltip',
-  props: {},
+  props: {
+    raEffect: {
+      type: String as PropType<TToolTipProps>,
+      default: 'light',
+    },
+    raContent: {
+      type: String,
+      default: '',
+    },
+    raPlacement: {
+      type: String as PropType<TPlacement>,
+      default: 'bottom',
+    },
+    raDisabled: {
+      type: Boolean,
+      default: false,
+    },
+    raOffset: {
+      type: Number,
+      default: 0,
+    },
+    raVisibleArrow: {
+      type: Boolean,
+      default: true,
+    },
+  },
   setup(props) {
     // init here
   },
